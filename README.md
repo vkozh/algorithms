8d5e96a59891b1183c9e8e91b6ac3abc7c3e70d0
Lekozhe <v.a.kozh@ya.ru>
2022-04-11 18:40:57 +0300

    bubble

---
+++ README.md
@@ -0,0 +1,17 @@
+Тренировка алгоритмов с тестированием на Jest
+
+1. Сортировка
+- хитрая сортировка
+<!-- - пузырек
+- шейкерная
+- расческой
+- вставками
+- выбором
+- быстрая
+- слиянием
+- пирамидальная -->
+
+Источники:
+- https://blog.unilecs.org/tricky-sort-solution
+- https://academy.yandex.ru/posts/osnovnye-vidy-sortirovok-i-primery-ikh-realizatsii
+- https://tproger.ru/articles/computational-complexity-explained/
---
+++ src/sorting.test.js
@@ -0,0 +1,41 @@
+const {
+    test,
+    expect
+} = require('@jest/globals');
+const {
+    assert
+} = require('chai');
+const sort = require('./sorting');
+
+describe('test jest', () => {
+    it('adds 1 + 2 to equal 3', () => {
+        assert.equal(sort.sum(1, 2), 3);
+    });
+})
+
+describe('tricky sorting', () => {
+    test('[] to equal ""', () => {
+        expect(sort.tricky([])).toEqual('')
+    })
+    test('[1, 1] to equal "11"', () => {
+        expect(sort.tricky([1, 1])).toEqual('11')
+    })
+    test('["123", "124", "56", "90"] to equal "9056124123"', () => {
+        expect(sort.tricky(["123", "124", "56", "90"])).toEqual("9056124123");
+    })
+    test('["123", "124", "56", "90", "9"] to equal "99056124123"', () => {
+        expect(sort.tricky(["123", "124", "56", "90", "9"])).toEqual("99056124123");
+    })
+    test('["123", "127", "1239"] to equal "1271239123"', () => {
+        expect(sort.tricky(["123", "127", "1239"])).toEqual("1271239123");
+    })
+})
+
+describe('bubble sorting', () => {
+    test('lazy: [40, 100, 1, 5, 25, 10] to equal [1, 5, 10, 25, 40, 100]', () => {
+        expect(sort.bubble([40, 100, 1, 5, 25, 10])).toEqual([1, 5, 10, 25, 40, 100])
+    })
+    test('[40, 100, 1, 5, 25, 10] to equal [1, 5, 10, 25, 40, 100]', () => {
+        expect(sort.bubble_([40, 100, 1, 5, 25, 10])).toEqual([1, 5, 10, 25, 40, 100])
+    })
+})
